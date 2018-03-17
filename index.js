dd6eb79e6a1b1e54fadd65befb73f7bf5ffbdc6a
Asher Ferri <asherferri85@gmail.com>
2018-03-17 00:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-function theBeatlesPlay(instruments, musicians) {
-var string = [];
-	
-
-for (e=0; e <musicians.length; e++) {
-	string[e] = "he";
- 		}
-
- 	for (i=0; i<musicians.length; i++) {
- 		string[i] += (`musicians[i] + " plays " + instruments[i]`) ;
-	}
-	
-return string;
+function theBeatlesPlay(musicians,instruments ){
+  var emptyarray=[];
+  for (var i=0;i<musicians.length;i++){
+  emptyarray[i]=(musicians[i]+ " plays " +instruments[i]);
+  }
+  return emptyarray;
 }
- theBeatlesPlay(["Why", "No"] , ["Guitar", "Hello"]);
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  var arrayfacts=[];
+  var i=0;
+  while (i<facts.length) {
+  arrayfacts[i]=facts[i]+"!!!";
+  i++;
+  }
+  return arrayfacts;
+}
+
+
+function iLoveTheBeatles (number){
+  var ihatethebeatles=[];
+  do {
+  ihatethebeatles.push("I love the Beatles!");
+  number++;
+  } while (number<15 );
+  return ihatethebeatles;
+}
